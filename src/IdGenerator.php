6d801b7362a9b6c7b008f4e1455bf04758563af0
XAKEPEHOK <icq-475204@ya.ru>
2020-04-03 03:36:54 +0300

    init

---
+++ src/IdGenerator.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created for id-generator-redis
+ * Date: 31.03.2020
+ * @author Timur Kasumov (XAKEPEHOK)
+ */
+
+namespace DiBify\IdGenerator\Redis;
+
+
+use DiBify\DiBify\Id\Id;
+use DiBify\DiBify\Id\IdGeneratorInterface;
+use DiBify\DiBify\Model\ModelInterface;
+use Redis;
+
+class IdGenerator implements IdGeneratorInterface
+{
+
+    private $key;
+    private $redis;
+
+    public function __construct(Redis $redis, $key = 'RedisIdGenerator')
+    {
+        $this->key = $key;
+        $this->redis = $redis;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function __invoke(ModelInterface $model): Id
+    {
+        if (!$model->id()->isAssigned()) {
+            $model->id()->assign($this->getRedis()->hIncrBy($this->getKey(), $model::getModelAlias(), 1));
+        }
+        return $model->id();
+    }
+
+    /**
+     * @param string $modelAlias
+     * @param int $current
+     */
+    public function setCounterValue(string $modelAlias, int $current)
+    {
+        $this->getRedis()->hSet($this->getKey(), $modelAlias, $current);
+    }
+
+    protected function getRedis(): Redis
+    {
+        return $this->redis;
+    }
+
+    protected function getKey(): string
+    {
+        return $this->key;
+    }
+
+}
---
+++ tests/IdGeneratorTest.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Created for id-generator-redis
+ * Date: 03.04.2020
+ * @author Timur Kasumov (XAKEPEHOK)
+ */
+
+namespace DiBify\IdGenerator\Redis;
+
+use DiBify\DiBify\Id\Id;
+use DiBify\DiBify\Model\ModelInterface;
+use PHPUnit\Framework\TestCase;
+use Redis;
+
+class IdGeneratorTest extends TestCase
+{
+
+    /** @var Redis */
+    private $redis;
+
+    /** @var string */
+    private $key;
+
+    /** @var IdGenerator */
+    private $generator;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->redis = new Redis();
+        $this->redis->connect('127.0.0.1', 6379);
+        $this->redis->select(0);
+
+        $this->key = 'IdGenerator';
+        $this->redis->del($this->key);
+
+        $this->generator = new IdGenerator(
+            $this->redis,
+            $this->key
+        );
+    }
+
+    public function test__invoke()
+    {
+        $foo_1 = $this->getFooModel();
+        $this->assertFalse($foo_1->id()->isAssigned());
+        $id = ($this->generator)($foo_1);
+        $this->assertSame($foo_1->id(), $id);
+        $this->assertSame('1', (string) $id);
+
+        $foo_2 = $this->getFooModel();
+        $this->assertFalse($foo_2->id()->isAssigned());
+        $id = ($this->generator)($foo_2);
+        $this->assertSame($foo_2->id(), $id);
+        $this->assertSame('2', (string) $id);
+
+        $bar_1 = $this->getBarModel();
+        $this->assertFalse($bar_1->id()->isAssigned());
+        $id = ($this->generator)($bar_1);
+        $this->assertSame($bar_1->id(), $id);
+        $this->assertSame('1', (string) $id);
+
+        $bar_2 = $this->getBarModel();
+        $this->assertFalse($bar_2->id()->isAssigned());
+        $id = ($this->generator)($bar_2);
+        $this->assertSame($bar_2->id(), $id);
+        $this->assertSame('2', (string) $id);
+    }
+
+    public function testSetCounterValue()
+    {
+        $foo = $this->getFooModel();
+        $this->generator->setCounterValue($foo::getModelAlias(), 100);
+        $this->assertFalse($foo->id()->isAssigned());
+        $id = ($this->generator)($foo);
+        $this->assertSame($foo->id(), $id);
+        $this->assertSame('101', (string) $id);
+    }
+
+    private function getFooModel(): ModelInterface
+    {
+        return new class() implements ModelInterface {
+
+            /** @var Id */
+            private $id;
+
+            public function __construct()
+            {
+                $this->id = new Id();
+            }
+
+            public function id(): Id
+            {
+                return $this->id;
+            }
+
+            public static function getModelAlias(): string
+            {
+                return 'foo';
+            }
+        };
+    }
+
+    private function getBarModel(): ModelInterface
+    {
+        return new class() implements ModelInterface {
+
+            /** @var Id */
+            private $id;
+
+            public function __construct()
+            {
+                $this->id = new Id();
+            }
+
+            public function id(): Id
+            {
+                return $this->id;
+            }
+
+            public static function getModelAlias(): string
+            {
+                return 'bar';
+            }
+        };
+    }
+
+}
